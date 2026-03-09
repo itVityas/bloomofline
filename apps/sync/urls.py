5c9fff083909c8a485a3a692df3134dc6a223c96
Anton Kralin <kralin.dart@gmail.com>
2026-03-09 13:21:10 +0300

    add mssql state view

--- apps/sync/urls.py
+++ apps/sync/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from apps.sync.views.sync import FullSyncAllView, SyncAllView
+from apps.sync.views.global_state import GlobalStateView
 
 
 urlpatterns = [
     path('full_sync/', FullSyncAllView.as_view()),
     path('sync/', SyncAllView.as_view()),
+    path('mssqlstate/', GlobalStateView.as_view())
 ]
---
+++ apps/sync/views/global_state.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiResponse
+from rest_framework.response import Response
+
+from bloomofline.global_state import global_state
+
+
+@extend_schema(tags=['GlobalState'])
+@extend_schema_view(
+    get=extend_schema(
+        summary='Get global state',
+        responses={
+            200: OpenApiResponse(description='Return global state'),
+        },
+    ),
+)
+class GlobalStateView(APIView):
+    @staticmethod
+    def get(request):
+        return Response({'global_state': global_state.set()})
